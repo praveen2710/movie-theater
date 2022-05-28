db75f41bd0050e7ca5551f2f967ae7fc7fb2227c
PRAVEEN <praveenbathia27@gmail.com>
2022-05-28 17:42:53 -0400

    renamedTest and moved things around

--- src/test/java/com/jpmc/theater/MovieTests.java
+++ src/test/java/com/jpmc/theater/MovieTests.java
@@ -11,11 +11,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MovieTests {
     @Test
-    void specialMovieWith50PercentDiscount() {
+    void specialMovieWith20PercentDiscount() {
         Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90),12.5, 1);
-        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.now()));
-        assertEquals(10, spiderMan.calculateTicketPrice(showing));
-
-        System.out.println(Duration.ofMinutes(90));
+        Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.of(9, 0)));
+        assertEquals(spiderMan.getTicketPrice()*0.8, spiderMan.calculateTicketPrice(showing));
     }
+
+    @Test
+    void testTimedDiscount(){
+        var customer = new Customer("John Doe", "unused-id");
+        int audienceCount = 3;
+        Movie movie =  new Movie("Turning Red", Duration.ofMinutes(85), 11, 0);
+        var showing = new Showing(
+                movie,
+                4,
+                LocalDateTime.now().withHour(11).withMinute(0)
+        );
+        assertEquals( (movie.getTicketPrice()*0.75)*audienceCount,new Reservation(customer, showing, audienceCount).totalFee());
+    }
+
+    @Test
+    void testOverlappingDiscount(){
+        var customer = new Customer("John Doe", "unused-id");
+        int audienceCount = 3;
+        Movie movie =  new Movie("Turning Red", Duration.ofMinutes(85), 2, 1);
+        var showing = new Showing(
+                movie,
+                7,
+                LocalDateTime.now().withHour(11).withMinute(0)
+        );
+        double expectedTicketPrice = Math.min(movie.getTicketPrice()-1,movie.getTicketPrice()*0.75);
+        assertEquals( expectedTicketPrice*audienceCount,new Reservation(customer, showing, audienceCount).totalFee());
+    }
+
+
 }
--- src/test/java/com/jpmc/theater/ReservationTests.java
+++ src/test/java/com/jpmc/theater/ReservationTests.java
@@ -21,31 +21,4 @@ public class ReservationTests {
         );
         assertEquals( (12.5-3)*audienceCount,new Reservation(customer, showing, audienceCount).totalFee());
     }
-
-    @Test
-    void testTimedDiscount(){
-        var customer = new Customer("John Doe", "unused-id");
-        int audienceCount = 3;
-        Movie movie =  new Movie("Turning Red", Duration.ofMinutes(85), 11, 0);
-        var showing = new Showing(
-                movie,
-                4,
-                LocalDateTime.now().withHour(11).withMinute(0)
-        );
-        assertEquals( (movie.getTicketPrice()*0.75)*audienceCount,new Reservation(customer, showing, audienceCount).totalFee());
-    }
-
-    @Test
-    void testOverlappingDiscount(){
-        var customer = new Customer("John Doe", "unused-id");
-        int audienceCount = 3;
-        Movie movie =  new Movie("Turning Red", Duration.ofMinutes(85), 2, 1);
-        var showing = new Showing(
-                movie,
-                7,
-                LocalDateTime.now().withHour(11).withMinute(0)
-        );
-        double expectedTicketPrice = Math.min(movie.getTicketPrice()-1,movie.getTicketPrice()*0.75);
-        assertEquals( expectedTicketPrice*audienceCount,new Reservation(customer, showing, audienceCount).totalFee());
-    }
 }
